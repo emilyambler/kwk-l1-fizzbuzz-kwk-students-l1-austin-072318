264fe2a43cbfecffa0ab558e0f73956724abec7e
emilyambler <emilyambler4@gmail.com>
2018-07-24 18:16:12 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -9,3 +9,11 @@ end
 end
 
 fizzbuzz(15)
+
+loop do
+  puts "to the right, to the right, to the right, to the right"
+  puts "to the left, to the left, to the left, to the left"
+  puts "now kick! now kick! now kick! now kick!"
+  puts "now walk it by yourself, now walk it by yourself"
+  
+end
